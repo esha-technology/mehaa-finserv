f94c241b3504ba2786c95141562fc35a69ff05e4
srikar kudurmalla <srikar2985969c@gmail.com>
2025-01-03 11:38:03 +0530

    loan form removed from loans page

--- app/loans/page.tsx
+++ app/loans/page.tsx
@@ -1,13 +1,15 @@
 "use client"
+import Footer from "../components/common/Footer";
 import NavBar from "../components/common/Navbar";
 import Loan from "../components/Loan";
-import LoanForm from "../components/LoanForm";
+
+
 const Loansection: React.FC = () => {
   return (
     <div>
       <NavBar/>
       <Loan/>
-      <LoanForm/>
+      <Footer/>
       </div>
   );
 };
