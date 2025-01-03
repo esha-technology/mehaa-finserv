222a695dfcab006ad4944cc3a5b0f5d3f8ccc399
charan062000 <jarupulacharannayak@gmail.com>
2025-01-03 22:53:10 +0530

    machine

--- app/loans/machinery-loan/page.tsx
+++ app/loans/machinery-loan/page.tsx
@@ -6,7 +6,7 @@ import LoanApplicationForm from "@/app/components/LoanForm";
 import NavBar from "@/app/components/common/Navbar";
 import Whyus from "@/app/components/Whyus";
 import Footer from "@/app/components/common/Footer";
-import { GiGears, GiReceiveMoney } from "react-icons/gi";
+import { GiGears} from "react-icons/gi";
  
 
 const MachineryLoan = () => {
